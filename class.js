01d60920f7ec8a4547322b3b63a9e529b684c666
Razai Zim <zim.sarkar5956@gmail.com>
2022-10-27 06:44:24 +0600

    JS Class

---
+++ class.js
@@ -0,0 +1,14 @@
+class Student {
+    constructor(sID, nameId){
+    this.id = sID;
+    this.name = nameId;
+    this.school = "Phulbari G.M."
+    }
+}
+
+const student1 = new Student(13, "Rokib");
+const student2 = new Student(15, "Rofiq");
+
+console.log(student1);
+console.log(student2);
+
